87cfadd7ba0035e6c8407f55933969999a5bf91e
Daniel Mbaluka <danielmbaluka@gmail.com>
2019-03-04 14:28:39 +0300

    bug fix

--- authentication/middleware.py
+++ authentication/middleware.py
@@ -11,8 +11,6 @@ class ForceResetPasswordMiddleware(MiddlewareMixin):
         if (request.user.is_authenticated()
             and re.match(r"^/admin/?", request.path)
             and (request.session.get("force_reset_password", False))
-            and not re.match(r"/admin/password_change|/admin/logout|/admin/password_change/done", request.path)
+            and not re.match(r"/admin/password_change|/admin/logout", request.path)
             ):
-            print("redirecting user to change their password")
-            print(request.session.get("force_reset_password"))
             return HttpResponseRedirect(reverse("admin:password_change"))
